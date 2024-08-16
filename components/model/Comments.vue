349f71c8789ee1f305164a225bcc3da2b4895359
Turarbek-2005 <turarbek.kabdesh@mail.ru>
2024-08-17 01:12:03 +0500

    added full adaptive

--- components/model/Comments.vue
+++ components/model/Comments.vue
@@ -1,8 +1,8 @@
 <script lang="ts" setup>
-import dayjs from 'dayjs';
-import type { IPublication } from '@/types/publication.types';
-import { useComments } from './useComments';
-import { useCreateComment } from './useCreateComment';
+import dayjs from "dayjs";
+import type { IPublication } from "@/types/publication.types";
+import { useComments } from "./useComments";
+import { useCreateComment } from "./useCreateComment";
 
 const { data, refetch, isLoading } = useComments();
 const { commentRef, writeComment } = useCreateComment({ refetch });
@@ -11,38 +11,33 @@ const card = data as unknown as IPublication;
 </script>
 
 <template>
-    <div class="w-2/4">
-        <Input
-            placeholder="Оставьте комментарий"
-            v-model="commentRef"
-            @keyup.enter="writeComment"
-            class="w-full"
-        />
-        <Button class="mt-4" @click="writeComment">Отправить</Button>
-        <Skeleton v-if="isLoading" class="w-full h-[76px] rounded mt-5" />
-        <div v-else-if="card" class="overflow-y-scroll h-2/3 mt-3">
-            <div
-                v-for="comment in card?.comments"
-                :key="comment.$id"
-                class="flex items-center mt-5"
-            >
-                <NuxtImg
-                    :src="comment.avatar"
-                    class="rounded-full mr-3 h-12 w-12"
-                />
-                <div class="border-border bg-black/20 rounded w-full">
-                    <div class="mb-2 text-xl">
-                        {{ comment.name }}
-                        <!-- {{ dayjs(comment.$createdAt).format('') }} -->
-                        <label class="text-zinc-400 text-sm">{{
-                            dayjs(comment.$createdAt).format(
-                                'DD MMMM YYYY HH:mm'
-                            )
-                        }}</label>
-                    </div>
-                    <p>{{ comment.text }}</p>
-                </div>
-            </div>
+  <div class="w-full lg:w-2/4">
+    <Input
+      placeholder="Оставьте комментарий"
+      v-model="commentRef"
+      @keyup.enter="writeComment"
+      class="w-full"
+    />
+    <Button class="mt-4" @click="writeComment">Отправить</Button>
+    <Skeleton v-if="isLoading" class="w-full h-[76px] rounded mt-5" />
+    <div v-else-if="card" class="overflow-y-scroll h-2/3 mt-3">
+      <div
+        v-for="comment in card?.comments"
+        :key="comment.$id"
+        class="flex items-center mt-5"
+      >
+        <NuxtImg :src="comment.avatar" class="rounded-full mr-3 h-12 w-12" />
+        <div class="border-border bg-black/20 rounded w-full">
+          <div class="mb-2 text-xl">
+            {{ comment.name }}
+            <!-- {{ dayjs(comment.$createdAt).format('') }} -->
+            <label class="text-zinc-400 text-sm">{{
+              dayjs(comment.$createdAt).format("DD MMMM YYYY HH:mm")
+            }}</label>
+          </div>
+          <p>{{ comment.text }}</p>
         </div>
+      </div>
     </div>
+  </div>
 </template>
--- components/model/Model.vue
+++ components/model/Model.vue
@@ -1,44 +1,68 @@
 <script setup lang="ts">
-import { useDialogSlideStore } from '~/store/dialog-slide.store';
+import { useDialogSlideStore } from "~/store/dialog-slide.store";
 
 const store = useDialogSlideStore();
 </script>
 <template>
-    <DialogContent class="dark:text-white">
-        <div class="flex items-center mb-3">
-            <NuxtImg
-                :src="store.card?.avatar"
-                width="50"
-                class="rounded-full mr-3 h-14 w-14"
-            />
-            <h4 class="dark:text-white text-xl mr-3">{{ store.card?.name }}</h4>
-            <!-- <p class="dark:text-white">
-                    {{ dayjs(store.card?.$createdAt).format('DD MMMM YYYY') }}
-                </p> -->
-        </div>
-        <div class="flex">
-            <Carousel class="relative carousel ml-8 mr-14">
-                <CarouselContent class="relative">
-                    <CarouselItem
-                        v-for="photo in store.card?.photoes"
-                        :key="store.card?.$id"
-                    >
-                        <NuxtImg class="rounded w-full img" :src="photo" />
-                    </CarouselItem>
-                </CarouselContent>
-                <CarouselPrevious class="dark:text-white" />
-                <CarouselNext class="dark:text-white" />
-            </Carousel>
-            <ModelComments />
-        </div>
-    </DialogContent>
+  <DialogContent class="dark:text-white overflow-scroll">
+    <div class="flex items-center mb-3">
+      <NuxtImg
+        :src="store.card?.avatar"
+        width="50"
+        class="rounded-full mr-3 h-14 w-14"
+      />
+      <h4 class="dark:text-white text-xl mr-3">{{ store.card?.name }}</h4>
+    </div>
+    <div class="flex flex-col lg:flex-row mx-auto lg:mx-0 w-2/3 lg:w-full">
+      <Carousel class="relative carousel mx-auto mb-5 lg:mb-0 lg:ml-8 lg:mr-14">
+        <CarouselContent class="relative">
+          <CarouselItem
+            v-for="photo in store.card?.photoes"
+            :key="store.card?.$id"
+          >
+            <NuxtImg class="rounded w-full img" :src="photo" />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious class="dark:text-white" />
+        <CarouselNext class="dark:text-white" />
+      </Carousel>
+      <ModelComments />
+    </div>
+  </DialogContent>
 </template>
 <style scoped>
 .carousel {
-    height: 30rem;
-    width: 25rem;
+  height: 30rem;
+  width: 25rem;
 }
 .img {
+  height: 30rem;
+}
+@media screen and (max-width: 1023px) {
+  .carousel {
     height: 30rem;
+    width: 100%;
+  }
+  .img {
+    height: 30rem;
+  }
+}
+@media screen and (max-width: 767px) {
+  .carousel {
+    height: 20rem;
+    width: 18rem;
+  }
+  .img {
+    height: 20rem;
+  }
+}
+@media screen and (max-width: 639px) {
+  .carousel {
+    height: 16rem;
+    width: 12rem;
+  }
+  .img {
+    height: 16rem;
+  }
 }
 </style>
